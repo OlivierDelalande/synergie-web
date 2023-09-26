6fe514e719840911b318c435993b488d67028b66
odelalande <o.delalande@proton.me>
2023-09-26 23:15:07 +0200

    setup eslint rules

--- back/synergieb/.eslintrc.js
+++ back/synergieb/.eslintrc.js
@@ -21,5 +21,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    complexity: ['error', { max: 4 }],
+    'import/no-duplicates': 'error',
+    'max-depth': ['error', { max: 2 }],
+    'max-params': ['error', { max: 4 }],
+    'max-statements-per-line': ['error', { max: 1 }],
+    'no-param-reassign': 'error',
+    'max-nested-callbacks': ['error', { max: 1 }],
   },
 };
